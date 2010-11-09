12179814c80555f6ac3acc24caea6fbffbe05e17
Nick Hodges <nickhodges@gmail.com>
2010-11-09 18:14:05 -0500

    Minor rearranging of code

--- DelphiProjectClean.dpr
+++ DelphiProjectClean.dpr
@@ -7,10 +7,7 @@ uses
 
 var
   DirectoryToClean: string;
-
-
   TotalFilesDeleted: integer;
-
 
 const
   TExtensionList: array [0 .. 7] of string = ('*.dcp', '*.dcu', '*.identcache', '*.local', '*.exe', '*.dll', '*.bpl', '*.dcp');
@@ -26,11 +23,13 @@ begin
   Result := ExtractFilePath(GetModuleFileNameStr);
 end;
 
-procedure CleanDirectory;
+function CleanDirectory: integer;
 var
   TempExtension: string;
   TempFilename: string;
+  TempTotal: integer;
 begin
+  TempTotal := 0;
   if TDirectory.Exists(DirectoryToClean) then
   begin
     WriteLn('Removing Delphi-produced files from ', DirectoryToClean, '...');
@@ -40,37 +39,42 @@ begin
       begin
         WriteLn('Deleting ', TempFilename, ' ....');
         TFile.Delete(TempFilename);
-        Inc(TotalFilesDeleted);
+        Inc(TempTotal);
       end
-    end;
-    if TotalFilesDeleted = 0 then
-    begin
-      WriteLn('No files found');
     end;
   end
   else
   begin
     WriteLn('Directory is invalid: ', DirectoryToClean);
   end;
-
-  WriteLn('Total files deleted: ', TotalFilesDeleted);
-  WriteLn('All Done.');
+  Result := TempTotal;
 end;
 
 begin
+ { TODO : Allow recursiveness to be a command line option }
   WriteLn('Delphi Directory Cleaning Utility Version 1.0');
   WriteLn('Copyright © 2010 by Nick Hodges All Rights Reserved');
-  TotalFilesDeleted := 0;
+//  TotalFilesDeleted := 0;
   if ParamCount > 0 then
   begin
     DirectoryToClean := ParamStr(1);
   end
   else
   begin
+    // if no directory is passed, attempt to clean the directory where the
+    // EXE resides
     DirectoryToClean := ModulePath;
   end;
-    Assert(DirectoryToClean <> '', 'Somehow the DirectoryToClean variable is empty.  That should be impossible.');
-      CleanDirectory;
+  Assert(DirectoryToClean <> '', 'Somehow the DirectoryToClean variable is empty.  That should be impossible.');
+  TotalFilesDeleted := CleanDirectory;
+
+  if TotalFilesDeleted = 0 then
+  begin
+    WriteLn('No files found');
+  end;
+
+  WriteLn('Total files deleted: ', TotalFilesDeleted);
+  WriteLn('All Done.');
 {$IFDEF DEBUG}
   Readln;
 {$ENDIF}
